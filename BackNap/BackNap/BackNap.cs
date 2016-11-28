0a742f9c2c589a785f1822f89e62dd5c76df7990
jkesh93 <jay111893@gmail.com>
2016-11-28 12:57:45 -0500

    Added the BackNap object in, set its properties and fixed the line reader

---
+++ BackNap/BackNap/BackNap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backNapObject
+{
+    public class BackNap
+    {
+        int maxProfit = 0;
+        int numbest = 0;
+
+        public BackNap(int numItems, int index, int profit, int weight, int ksWeight)
+        {
+            // If we're in the specified weight Range AND the profit is greater than our current best --> add this item;
+            if(weight <= ksWeight && profit > maxProfit)
+            {
+                maxProfit = profit;
+                numbest = index;
+                
+
+            }
+        }
+
+
+    }
+}
---
+++ knapSackProject/umf.cos364/HeapDriver2/HeapDriver2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using umf.cos364;  // do this and add Heap to References for HeapDriver
+
+namespace HeapDriver2
+{
+    class Program
+    {
+        public static int[] myList = { 20, 21, 18, 25, 22, 26, 29, 14, 15, 17};
+        static void Main(string[] args)
+        {
+            Heap myHeap = new Heap(30);
+
+            for (int i = 0; i < myList.Length; i++)
+                myHeap.enHeap(myList[i]);
+
+            int[] sortedList = new int[myList.Length + 1];
+
+            int j = myList.Length;
+            Console.WriteLine();
+            Console.WriteLine();
+            while (!myHeap.empty())
+                sortedList[j--] = myHeap.removeTop();
+
+
+            Console.WriteLine("The sorted array in increasing order:\n");
+
+            for (int i = 1; i < sortedList.Length; i++)
+                Console.Write(sortedList[i] + "  ");
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
